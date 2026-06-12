e0a6daa8e3d6fc1ca7869413c66398522e735807
Guilherme Souza <159570524+PurplebeltBr@users.noreply.github.com>
2026-06-11 23:35:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function scrollSlider(direction) {
     behavior: 'smooth'
   });
 }
+
+// Captura o movimento do mouse na tela inteira
+document.addEventListener('mousemove', (e) => {
+    // Define as variáveis --x e --y no elemento body com a posição atual do cursor
+    document.body.style.setProperty('--x', e.clientX + 'px');
+    document.body.style.setProperty('--y', e.clientY + 'px');
+});
